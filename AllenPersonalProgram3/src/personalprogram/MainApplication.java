9fcae23cf6d31c692650acdfa171d1944b1f5282
Gamerbro33 <rjallen2501@gmail.com>
2024-07-20 16:23:24 -0400

    adding the files to github to first commit

---
+++ AllenPersonalProgram3/src/personalprogram/MainApplication.java
@@ -0,0 +1,20 @@
+package personalprogram;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import personalprogram.model.Store;
+import personalprogram.view.StartupWindow;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		StartupWindow window = new StartupWindow(primaryStage);
+		window.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
